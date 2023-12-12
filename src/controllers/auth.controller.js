8994cdb9052e377095cf3a53a0be884369d77f2f
Luis Aguilar (lr88090) <aguilarluisdavid0@gmail.com>
2023-12-12 00:12:18 -0600

    se agrega cifrado de password y generacion de token en signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,35 @@
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
 import User from './../models/user.model.js'
 
 export const register = async (req, res) => {
   console.log(req.body)
   const { username, email, password } = req.body
   try {
+    const passwordHash = await bcrypt.hash(password, 10)
     const newUser = new User({
       username,
       email,
-      password
+      password: passwordHash
     })
     await newUser.save()
     const userSaved = await newUser.save()
-    res.status(201).json(userSaved)
+    const token = await createAccessToken({ id: userSaved._id })
+    res.cookie('token', token)
+    res.status(201).json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+      message: 'User registered successfully'
+    })
   } catch (error) {
     console.log('An error ocurred in register', error)
+    res.status(500).json({
+      message: 'An error ocurred in register',
+      error: error.message
+    })
   }
 }
 
