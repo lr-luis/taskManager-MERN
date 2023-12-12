f802dc1b8331ce4f813aa016aa640aec8cf0235a
Luis Aguilar (lr88090) <aguilarluisdavid0@gmail.com>
2023-12-12 11:13:57 -0600

    se crea rutas para login y logut

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,5 +34,42 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-  res.send('login')
+  console.log(req.body)
+  const { email, password } = req.body
+  try {
+    const userFound = await User.findOne({ email })
+    if (!userFound) {
+      return res.status(400).json({ message: 'User not found' })
+    }
+
+    const isMatch = await bcrypt.compare(password, userFound.password)
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Incorrect password' })
+    }
+
+    const token = await createAccessToken({ id: userFound._id })
+    res.cookie('token', token)
+    res.status(201).json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+      message: 'User registered successfully'
+    })
+  } catch (error) {
+    console.log('An error ocurred in register', error)
+    res.status(500).json({
+      message: 'An error ocurred in register',
+      error: error.message
+    })
+  }
+}
+
+export const logout = async (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0)
+  })
+
+  return res.sendStatus(200)
 }
