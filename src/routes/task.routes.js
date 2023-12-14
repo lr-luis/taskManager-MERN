4a4a4783013ec78e15732f4dc9dfe1d7145985c1
Luis Aguilar (lr88090) <aguilarluisdavid0@gmail.com>
2023-12-13 23:56:13 -0600

    se agregan validaciones con zod

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,13 +1,15 @@
-import { Router } from 'express';
-import { createTask, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js';
-import { authRequired } from '../middlewares/validateToken.js';
+import { Router } from 'express'
+import { createTask, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js'
+import { authRequired } from '../middlewares/validateToken.js'
+import { validateSchema } from '../middlewares/validator.middleware.js'
+import { createTaskSchema } from '../schemas/task.schema.js'
 
-const router = Router();
+const router = Router()
 
-router.get('/tasks', authRequired, getTasks);
-router.get('/tasks/:id', authRequired, getTask);
-router.post('/tasks', authRequired, createTask)
+router.get('/tasks', authRequired, getTasks)
+router.get('/tasks/:id', authRequired, getTask)
+router.post('/tasks', authRequired, validateSchema(createTaskSchema), createTask)
 router.delete('/tasks:id', authRequired, deleteTask)
 router.put('/tasks/:id', authRequired, updateTask)
 
-export default router;
+export default router
