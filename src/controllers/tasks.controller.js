d2738161aeed172a4d1acbff25d33b1a2d0dd9c6
Luis Aguilar (lr88090) <aguilarluisdavid0@gmail.com>
2023-12-13 23:13:01 -0600

    se agrega rutas y modelos para el CRUD de tareas

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,50 @@
+import Task from '../models/task.model.js'
+
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({
+    user: req.user.id
+  }).populate('user')
+  res.json(tasks)
+}
+
+export const createTask = async (req, res) => {
+  const { title, description, date } = req.body
+  const newTask = new Task({
+    title,
+    description,
+    date,
+    user: req.user.id
+  })
+
+  const taskSaved = await newTask.save()
+  res.status(201).json(taskSaved)
+}
+
+export const getTask = async (req, res) => {
+  const { id } = req.params
+  const taskFound = await Task.findById(id).populate('user')
+  if (!taskFound) {
+    return res.status(404).json({ message: 'Task not found' })
+  }
+  res.status(200).json(taskFound)
+}
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params
+  const taskDeleted = await Task.findByIdAndDelete(id)
+  if (!taskDeleted) {
+    return res.status(404).json({ message: 'Task not found' })
+  }
+  return res.status(204)
+}
+
+export const updateTask = async (req, res) => {
+  const taskUpdated = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true // to return the new task instead of the old one
+  })
+  if (!taskUpdated) {
+    return res.status(404).json({ message: 'Task not found' })
+  }
+  console.log(taskUpdated)
+  res.status(202).json(taskUpdated)
+}
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { createTask, deleteTask, getTask, getTasks, updateTask } from '../controllers/tasks.controller.js';
 import { authRequired } from '../middlewares/validateToken.js';
 
 const router = Router();
 
-router.get('/tasks', authRequired, (req, res) => {
-    res.send('protected tasks');
-});
+router.get('/tasks', authRequired, getTasks);
+router.get('/tasks/:id', authRequired, getTask);
+router.post('/tasks', authRequired, createTask)
+router.delete('/tasks:id', authRequired, deleteTask)
+router.put('/tasks/:id', authRequired, updateTask)
 
 export default router;
